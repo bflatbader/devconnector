db0eac1ae1d208887a828c0b89e48943e80f2ae7
Bishop Bader <bflatbader@users.noreply.github.com>
2019-12-20 18:24:43 -0500

    added validation for user route

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 // Connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 // Define routes
